3603b0bb6be12a2dcadd3fc6ded8f416f16e9bca
saif-gitreps <110230611+saif-gitreps@users.noreply.github.com>
2023-03-06 19:52:24 +0600

    firstButton

---
+++ FirstButton.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+public class ClassDisplay {
+    public static void main(String[] args){
+       JFrame frame = new JFrame("simple button");
+       JButton button = new JButton("click me");
+       button.setSize(200,400);
+       frame.setSize(500,600);
+       frame.setBackground(Color.cyan);
+       frame.getContentPane().add(button);
+       frame.setVisible(true);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}

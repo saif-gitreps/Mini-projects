5994088234101fdc18bf483ea2eddc4cf2b00286
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-03-21 12:42:57 +0600

    Added new features and dimension  21/03/2023
    Features added, fields are invisible until right password is set, fields are uneditable and password fields disappear after confirmation.

--- Hotel_Reservation_Sys/AdminPage.java
+++ Hotel_Reservation_Sys/AdminPage.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
 public class AdminPage extends JFrame{
     public JPanel AdminPanel;
@@ -12,8 +11,25 @@ public class AdminPage extends JFrame{
     private JTextField CustomerNumber;
     private JTextField profitField;
     private JLabel bedLabel;
-    public ArrayList<Customer> customers;
-    public AdminPage(ArrayList<Customer> customers) {
+    private JLabel customerLabel;
+    private JLabel profitLabel;
+    private JLabel HotelLabel;
+    private JButton backLOGIN;
+    private JButton backPROGRAM;
+
+    public AdminPage(Customer customer) {
+        setSize(470,260);
+        setContentPane(getAdminPanel());
+        setTitle("ADMIN PANEL");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setVisible(true);
+        BedField.setVisible(false);
+        CustomerNumber.setVisible(false);
+        profitField.setVisible(false);
+        profitLabel.setVisible(false);
+        customerLabel.setVisible(false);
+        bedLabel.setVisible(false);
+
         enterButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -22,11 +38,41 @@ public class AdminPage extends JFrame{
                     JOptionPane.showMessageDialog(null,"Sorry you are not allowed.");
                 }
                 else{
+                    enterButton.setVisible(false);
+                    passwordField1.setVisible(false);
+                    passLabel.setVisible(false);
+                    BedField.setVisible(true);
+                    BedField.setEditable(false);
+                    CustomerNumber.setVisible(true);
+                    CustomerNumber.setEditable(false);
+                    profitField.setVisible(true);
+                    profitField.setEditable(false);
+                    profitLabel.setVisible(true);
+                    customerLabel.setVisible(true);
+                    bedLabel.setVisible(true);
 
+                    BedField.setText(String.valueOf(customer.bedOccupied));
+                    CustomerNumber.setText(String.valueOf(customer.totalMembers));
+                    profitField.setText(String.valueOf(customer.finalCost));
                 }
             }
         });
-
+        backLOGIN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                LoginPage login = new LoginPage();
+                login.setVisible(true);
+                dispose();
+            }
+        });
+        backPROGRAM.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ProgramPage program = new ProgramPage();
+                program.setVisible(true);
+                dispose();
+            }
+        });
 
     }
     public JPanel getAdminPanel(){

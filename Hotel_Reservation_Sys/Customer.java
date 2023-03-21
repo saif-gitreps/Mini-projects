17a7c6dd44c8140692dc61f40d9950bbf7706d8a
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-03-21 12:41:14 +0600

    Changed constructor arguments 21/03/2023
    added static variables for easy access around the entire program.

--- Hotel_Reservation_Sys/Customer.java
+++ Hotel_Reservation_Sys/Customer.java
@@ -1,20 +1,16 @@
 public class Customer {
-    String name;
-    String national;
-    String members;
-    String cred;
-    String bed;
-    String duration;
-    boolean food = false;
-    boolean tour = false;
-    public Customer(String name,String national,String members,String cred,String bed,String duration, boolean food,boolean tour){
-        this.name = name;
-        this.national = national;
+    int members;
+    int bed;
+    int totalCost;
+    public static int bedOccupied =0;
+    public static int totalMembers =0;
+    public static int finalCost =0;
+    public Customer(int members,int bed,int totalCost){
         this.members = members;
-        this.cred = cred;
         this.bed = bed;
-        this.duration = duration;
-        this.food = food;
-        this.tour = tour;
+        this.totalCost = totalCost;
+        bedOccupied += bed;
+        totalMembers += members;
+        finalCost += totalCost;
     }
 }

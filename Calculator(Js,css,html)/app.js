8dc26c6ed38bcef0b8a6fabf30a8b167becb5d65
Saif <droconos99@gmail.com>
2023-08-10 23:59:50 +0600

    added clear button

---
+++ Calculator(Js,css,html)/app.js
@@ -0,0 +1,8 @@
+//buttons
+const ACbutton = document.getElementById("AC-button");
+const EqualButton = document.getElementById("equal-button");
+
+//callback-functions
+function clearInput
+//event listeners;
+ACbutton.addEventListener("click",clearInput);

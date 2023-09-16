a19b9a3f886f9e779a90e6e88cbb03ab593f9528
Saif <droconos99@gmail.com>
2023-09-16 21:20:29 +0600

    added EJS syntax and javascript logic to render JSON files

--- Note-Pad/app.js
+++ Note-Pad/app.js
@@ -5,14 +5,17 @@ const path = require("path");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/home", (request, response) => {
-   const filePath = path.join(__dirname, "views", "index.html");
-   response.sendFile(filePath);
+   const filePath = path.join(__dirname, "note-data", "data.json");
+   const fileData = fs.readFileSync(filePath);
+   const existingData = JSON.parse(fileData);
+   response.render("index", { noteStored: existingData });
 });
 app.post("/note", (request, response) => {
    const requestData = request.body;
-
-   console.log(requestData);
 
    const filePath = path.join(__dirname, "note-data", "data.json");
    const fileData = fs.readFileSync(filePath);

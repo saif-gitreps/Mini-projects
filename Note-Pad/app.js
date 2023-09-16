cae1bb0173d18b13b00b5ba63de5c71e5251d4b1
Saif <droconos99@gmail.com>
2023-09-16 19:38:29 +0600

    successfuly added form submission to json file

--- Note-Pad/app.js
+++ Note-Pad/app.js
@@ -1,8 +1,26 @@
+const fs = require("fs");
 const express = require("express");
 const path = require("path");
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (request, response) => {});
+app.get("/home", (request, response) => {
+   const filePath = path.join(__dirname, "views", "index.html");
+   response.sendFile(filePath);
+});
+app.post("/note", (request, response) => {
+   const requestData = request.body;
+
+   console.log(requestData);
+
+   const filePath = path.join(__dirname, "note-data", "data.json");
+   const fileData = fs.readFileSync(filePath);
+   const existingData = JSON.parse(fileData);
+
+   existingData.push(requestData);
+   fs.writeFileSync(filePath, JSON.stringify(existingData));
+   response.redirect("/home");
+});
 
 app.listen(3000);

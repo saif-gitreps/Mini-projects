4503d19f8476db7ae49425759317d36c11b98609
Saif <droconos99@gmail.com>
2023-09-17 22:26:19 +0600

    completed all basic functionality of note-pad project

--- Note-Pad/app.js
+++ Note-Pad/app.js
@@ -15,6 +15,38 @@ app.get("/home", (request, response) => {
    const existingData = JSON.parse(fileData);
    response.render("index", { noteStored: existingData });
 });
+
+app.post("/deleting", (request, response) => {
+   const requestData = request.body.indexNumber;
+   console.log(typeof requestData);
+   const filePath = path.join(__dirname, "note-data", "data.json");
+   const fileData = fs.readFileSync(filePath);
+   const existingData = JSON.parse(fileData);
+
+   if (typeof requestData == "string") {
+      let index = 0;
+      for (let item of existingData) {
+         if (requestData == item.title) {
+            existingData.splice(index, 1);
+         }
+         index++;
+      }
+      fs.writeFileSync(filePath, JSON.stringify(existingData));
+      response.redirect("/home");
+   }
+   for (let deleteTitle of requestData) {
+      let index = 0;
+      for (let item of existingData) {
+         if (deleteTitle == item.title) {
+            existingData.splice(index, 1);
+         }
+         index++;
+      }
+   }
+   fs.writeFileSync(filePath, JSON.stringify(existingData));
+   response.redirect("/home");
+});
+
 app.post("/note", (request, response) => {
    const requestData = request.body;
 
--- Note-Pad/public/app/config.js
+++ Note-Pad/public/app/config.js
@@ -1,36 +1,31 @@
 const listItemsArray = document.querySelectorAll("display-note ul li");
 const navbar = document.querySelector("nav");
-const deleteButton = document.getElementById("delete-button");
-const selectButton = document.getElementById("select-button");
-const selectAllButton = document.getElementById("select-all-button");
+const cancelButton = document.getElementById("cancel-li");
+const selectButton = document.getElementById("select-li");
+const selectAllButton = document.getElementById("select-all-li");
 const checkBox = document.querySelectorAll(".display-checkbox");
-console.dir(navbar);
 
-if (listItemsArray.length != 0) {
-   navbar.style.display = "none";
-}
-
-let toggleCounter = 0;
 selectButton.addEventListener("click", () => {
-   if (toggleCounter % 2 == 0) {
-      deleteButton.style.display = "none";
-      selectAllButton.style.display = "none";
-      for (let item of checkBox) {
-         item.style.display = "none";
-      }
-   } else {
-      deleteButton.style.display = "block";
-      selectAllButton.style.display = "block";
-      for (let item of checkBox) {
-         item.style.display = "block";
-      }
-      for (let item of checkBox) {
-         if (item.checked == true) {
-            item.checked = false;
-         }
+   cancelButton.style.display = "block";
+   selectButton.style.display = "none";
+   selectAllButton.style.display = "block";
+   for (let item of checkBox) {
+      item.style.display = "block";
+   }
+});
+
+cancelButton.addEventListener("click", () => {
+   cancelButton.style.display = "none";
+   selectButton.style.display = "block";
+   selectAllButton.style.display = "none";
+   for (let item of checkBox) {
+      item.style.display = "none";
+   }
+   for (let item of checkBox) {
+      if (item.checked == true) {
+         item.checked = false;
       }
    }
-   toggleCounter++;
 });
 
 selectAllButton.addEventListener("click", () => {
